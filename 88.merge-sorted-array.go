618a8bce0ec110e8ad3a597734e1905bab7fa503
George Gkioulis <ggkioulis@suse.com>
2022-10-30 18:25:53 +0100

    Add problem 88, merge-sorted-array

---
+++ 88.merge-sorted-array.go
@@ -0,0 +1,38 @@
+package main
+
+/*
+ * @lc app=leetcode id=88 lang=golang
+ *
+ * [88] Merge Sorted Array
+ */
+
+// @lc code=start
+func merge(nums1 []int, m int, nums2 []int, n int) {
+
+	i, j := 0, 0
+	res := []int{}
+
+	for i < m || j < n {
+		if i < m {
+			if j < n {
+				if nums1[i] < nums2[j] {
+					res = append(res, nums1[i])
+					i++
+				} else {
+					res = append(res, nums2[j])
+					j++
+				}
+			} else {
+				res = append(res, nums1[i])
+				i++
+			}
+		} else {
+			res = append(res, nums2[j])
+			j++
+		}
+	}
+
+	copy(nums1, res)
+}
+
+// @lc code=end

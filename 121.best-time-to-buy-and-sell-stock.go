daeca2fc8093e8d024a9ff9ebaa9ccdc7dbf5613
George Gkioulis <ggkioulis@suse.com>
2022-11-09 19:47:53 +0100

    Add problem 121, best time to buy and sell stock

---
+++ 121.best-time-to-buy-and-sell-stock.go
@@ -0,0 +1,26 @@
+package main
+
+/*
+ * @lc app=leetcode id=121 lang=golang
+ *
+ * [121] Best Time to Buy and Sell Stock
+ */
+
+// @lc code=start
+func maxProfit(prices []int) int {
+	min := 10000
+	profit := 0
+
+	for _, price := range prices {
+		if (price - min) > profit {
+			profit = price - min
+		}
+		if price < min {
+			min = price
+		}
+	}
+
+	return profit
+}
+
+// @lc code=end
